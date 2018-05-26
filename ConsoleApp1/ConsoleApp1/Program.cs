1a6f5edb3bcf1e9e69390833a2313293c48de0dd
Elyor Latipov <elyor.blog@gmail.com>
2018-05-26 16:59:02 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object s1 = "abc";
+            string s2 = "abc";
+            bool result = s1.Equals(s2);
+
+
+            Console.WriteLine(result);
+
+            //
+        }
+    }
+}
